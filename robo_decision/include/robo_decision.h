abf7f183d2ec9e826e6c2556e8ae4923130fdbcb
Bowen Xie <bowen.x@robolution.ca>
2021-09-24 17:13:41 -0600

    Updated behaviours in RUNNING and RETURNING states

--- robo_decision/include/robo_decision.h
+++ robo_decision/include/robo_decision.h
@@ -42,46 +42,69 @@ namespace robo_decision
     nlohmann::json config_reader;
     ros::NodeHandle nh;
 
+    // Subscribers:
     ros::Subscriber cmd_vel_sub;
-    ros::Publisher cmd_vel_pub;
     ros::Subscriber user_command_sub;
     ros::Subscriber processing_sub;
     ros::Subscriber safety_sub;
+    ros::Subscriber hard_code_sub;
+    ros::Subscriber hard_code_cmd_vel_sub;
+
+    // Publishers:
+    ros::Publisher robo_decision_system_state_pub;
+    ros::Publisher cmd_vel_pub;
+
+    // Client:
     MoveBaseClient ac;
 
     State system_state = State::INIT;
 
+    // Main threads:
     void Run();
     void Run_excavation();
+
+    // Sub - callbacks:
     void Cmd_vel_callback(const geometry_msgs::Twist &msg);
     void User_command_callback(const std_msgs::Int8 &msg);
     void Processing_topic_callback(const geometry_msgs::PoseStamped &msg);
     void Safety_topic_callback(const std_msgs::Bool &msg);
+    void Hard_code_callback(const std_msgs::Bool &msg);
+    void Hard_code_cmd_vel_callback(const geometry_msgs::Twist &msg);
 
   private:
-    // Member varaibles
+    // Topic names
     std::string cmd_vel_sub_topic;
     std::string navigation_server_name;
     std::string user_command_topic;
     std::string processing_topic;
     std::string cmd_vel_pub_topic;
-
+    std::string robo_decision_system_state_topic;
+    std::string hard_code_topic;
+    std::string hard_code_cmd_vel_topic;
     std::string safety_topic;
+
+    // Navigation/cmd_vel
     move_base_msgs::MoveBaseGoal processed_naviation_goal =
         move_base_msgs::MoveBaseGoal();
     geometry_msgs::Twist stop_vehicle_twist = geometry_msgs::Twist();
     move_base_msgs::MoveBaseGoal return_to_home_goal =
         move_base_msgs::MoveBaseGoal();
-    int received_command = 0; // Defualt mode
-    bool system_inited = false;
     geometry_msgs::Twist received_cmd_vel;
-    bool safe_to_proceed = true; // TODO: check if safe to proceed?
+
+    // State variables
+    int received_command = 0; // Defualt mode
+    bool safe_to_proceed = true;
     bool found_target = false;
     bool excavation_mode = false;
+    bool resume_after_stop = true;
+    bool is_system_ok = false;
+    bool is_procecssed_navigation_goal_received = false;
+    bool is_cmd_vel_received = false;
 
     // Member functions
     void Init_return_to_home(const geometry_msgs::PoseStamped &home_location);
     void Init_stop_vehicle_twist();
+    bool Check_system_ok();
   };
 } // namespace robo_decision
 
--- robo_decision/src/robo_decision.cc
+++ robo_decision/src/robo_decision.cc
@@ -24,7 +24,12 @@ namespace robo_decision
       user_command_topic = config_reader["user_command_topic"];
       processing_topic = config_reader["processing_topic"];
       safety_topic = config_reader["safety_topic"];
+      robo_decision_system_state_topic =
+          config_reader["robo_decision_system_state_topic"];
       excavation_mode = bool(config_reader["excavation_mode"]);
+      hard_code_topic = config_reader["hard_code_topic"];
+      hard_code_cmd_vel_topic = config_reader["hard_code_cmd_vel_topic"];
+      resume_after_stop = bool(config_reader["resume_after_stop"]);
     }
   };
   RoboDecision::~RoboDecision() = default;
@@ -34,10 +39,17 @@ namespace robo_decision
     while (ros::ok())
     {
       // TODO: Complete the expression for different state:
+      is_system_ok = Check_system_ok();
       switch (system_state)
       {
       case State::INIT:
+
+        // set up publishers:
         cmd_vel_pub = nh.advertise<geometry_msgs::Twist>(cmd_vel_pub_topic, 1000);
+        robo_decision_system_state_pub =
+            nh.advertise<std_msgs::Int8>(robo_decision_system_state_topic, 1000);
+
+        // set up subscribers:
         cmd_vel_sub = nh.subscribe(cmd_vel_sub_topic, 1000,
                                    &RoboDecision::Cmd_vel_callback, this);
         user_command_sub = nh.subscribe(
@@ -47,39 +59,55 @@ namespace robo_decision
                          &RoboDecision::Processing_topic_callback, this);
         safety_sub = nh.subscribe(safety_topic, 1000,
                                   &RoboDecision::Safety_topic_callback, this);
+        hard_code_sub = nh.subscribe(hard_code_topic, 1000,
+                                     &RoboDecision::Hard_code_callback, this);
+        hard_code_cmd_vel_sub =
+            nh.subscribe(hard_code_cmd_vel_topic, 1000,
+                         &RoboDecision::Hard_code_cmd_vel_callback, this);
+
+        // Waiting for move_base action server to come up
         while (!ac.waitForServer(ros::Duration(5.0)) && ros::ok())
         {
           ROS_INFO("Waiting for the move_base action server to come up");
         }
         ROS_INFO("System initalized successfully.");
-        system_inited = true;
         ROS_INFO("Now waiting for incoming commands.");
         // Init_return_to_home();
         Init_stop_vehicle_twist();
+        is_system_ok = Check_system_ok();
         break;
 
       case State::RUNNING:
-        // check if the new navigation goal is received:
-        if (safe_to_proceed && system_inited)
+        if (!is_system_ok)
         {
-          cmd_vel_pub.publish(received_cmd_vel);
-          if (found_target ||
-              ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+          break;
+        }
+        // Check if hard_code mode is on
+        if (excavation_mode)
+        {
+          system_state = State::EXCAVATION;
+          ROS_INFO("Now entering into excavation mode");
+        }
+        if (is_procecssed_navigation_goal_received)
+        {
+          ROS_INFO("Updating Navigation Goal ...");
+          ac.cancelAllGoals();
+          ac.sendGoal(processed_naviation_goal);
+          if (is_cmd_vel_received && safe_to_proceed)
           {
-            ROS_INFO("Goal achieved ...");
-            ac.cancelAllGoals();
-            if (excavation_mode)
-            {
-              ROS_INFO("Now entering into excavation mode");
-              system_state = State::EXCAVATION;
-            }
+            cmd_vel_pub.publish(received_cmd_vel);
+          }
+          else if (!safe_to_proceed)
+          {
+            ROS_ERROR("Not safe to proceed"); // TODO: maybe add unsafe state?
+            cmd_vel_pub.publish(stop_vehicle_twist);
           }
         }
         break;
 
       case State::ABORT:
         /* system_stop = true; */
-        if (system_inited)
+        if (is_system_ok)
         {
           ROS_INFO("Abortiong task ...");
           ac.cancelAllGoals();
@@ -89,33 +117,49 @@ namespace robo_decision
         break;
 
       case State::RETURNNING:
-        if (safe_to_proceed && system_inited)
+        if (!is_system_ok)
+        {
+          break;
+        }
+        if (is_cmd_vel_received && safe_to_proceed)
         {
           cmd_vel_pub.publish(received_cmd_vel);
         }
-        break;
-
-      case State::EXCAVATION:
-        if (safe_to_proceed && system_inited)
+        else if (!safe_to_proceed)
         {
-          Run_excavation();
-        }
-        break;
-
-      default:
-        if (system_inited)
-        {
-          system_state = State::ABORT;
-          ac.cancelAllGoals();
+          ROS_ERROR("Not safe to proceed"); // TODO: maybe add unsafe state?
           cmd_vel_pub.publish(stop_vehicle_twist);
+
+          if (safe_to_proceed && is_system_ok)
+          {
+            cmd_vel_pub.publish(received_cmd_vel);
+          }
+          break;
+
+        case State::EXCAVATION:
+          // currently let other node to handle it.
+          // if (safe_to_proceed && is_system_ok)
+          // {
+          //   Run_excavation();
+          // }
+          break;
+
+        default:
+          if (is_system_ok)
+          {
+            system_state = State::ABORT;
+            ac.cancelAllGoals();
+            cmd_vel_pub.publish(stop_vehicle_twist);
+          }
         }
+        ros::spinOnce();
       }
-      ros::spinOnce();
     }
   }
 
   void RoboDecision::Cmd_vel_callback(const geometry_msgs::Twist &msg)
   {
+    is_cmd_vel_received = true;
     received_cmd_vel = msg;
   }
 
@@ -126,22 +170,22 @@ namespace robo_decision
     {
     case 1: // Task start
       ROS_INFO("Running command received ...");
-      if (system_inited)
+      if (is_system_ok)
       {
         system_state = State::RUNNING;
-        ac.cancelAllGoals();
-        ROS_INFO("Sending Navigation Goal ...");
-        ac.sendGoal(processed_naviation_goal);
+        is_procecssed_navigation_goal_received = false;
+        is_cmd_vel_received = false;
       }
       break;
 
     case 2: // Return home
       ROS_INFO("Returning home comand received ...");
-      if (system_inited)
+      if (is_system_ok)
       {
         system_state = State::RETURNNING;
         ac.cancelAllGoals();
         ROS_INFO("Now updaing navigation goal to home ...");
+        is_cmd_vel_received = false;
         ac.sendGoal(return_to_home_goal);
       }
       break;
@@ -160,6 +204,7 @@ namespace robo_decision
   void RoboDecision::Processing_topic_callback(
       const geometry_msgs::PoseStamped &msg)
   {
+    is_procecssed_navigation_goal_received = true;
     processed_naviation_goal.target_pose.header.frame_id = msg.header.frame_id;
     processed_naviation_goal.target_pose.header.stamp = ros::Time::now();
     processed_naviation_goal.target_pose.pose = msg.pose;
@@ -192,12 +237,35 @@ namespace robo_decision
   void RoboDecision::Safety_topic_callback(const std_msgs::Bool &msg)
   {
     safe_to_proceed = msg.data;
-    if (!safe_to_proceed)
+    if (!safe_to_proceed && resume_after_stop)
     {
       system_state = State::ABORT;
     }
   }
 
   // TODO: Hard-coded sequence
-  void RoboDecision::Run_excavation() {}
+  void RoboDecision::Run_excavation()
+  {
+  }
+
+  void RoboDecision::Hard_code_callback(const std_msgs::Bool &msg)
+  {
+    excavation_mode = bool(msg.data);
+  }
+
+  void RoboDecision::Hard_code_cmd_vel_callback(
+      const geometry_msgs::Twist &msg)
+  {
+    received_cmd_vel = msg;
+  }
+
+  // TODO: check system running status
+  bool RoboDecision::Check_system_ok()
+  {
+    if (!ac.isServerConnected())
+    {
+      return false;
+    }
+    return true;
+  }
 } // namespace robo_decision
